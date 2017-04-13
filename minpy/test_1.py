242d8eedaaa077a52de0dea45eb1d3639202b421
Yutian Li <hotpxless@gmail.com>
2017-04-12 23:18:35 -0700

    [minpy] Move test scripts.

--- minpy/test_1.py
+++
@@ -1,6 +0,0 @@
-import mxnet
-import numpy as np
-
-a = mxnet.nd.ones(2)
-b = mxnet.nd.ones(2)
-print((a + b).asnumpy())
--- minpy/test_2.py
+++
@@ -1,3 +0,0 @@
-import mxnet as mx
-mx.minpy.enable_jit()
-mx.minpy.disable_jit()
