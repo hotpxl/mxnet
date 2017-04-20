f9a641bf1460b50cd5f1144ee30dafafefc06e76
Haoran Wang <hrwangchengdu@hotmail.com>
2017-04-19 22:29:13 -0400

    [minpy] Update graph-equal test case.

--- minpy_test/test_4.py
+++ minpy_test/test_4.py
@@ -4,30 +4,37 @@ import time
 
 mx.minpy.enable_jit()
 
-print("Build G1")
+print("Run G1")
 a = mx.nd.ones(2)
 b = mx.nd.ones(2)
 c = a + b
 print(c.asnumpy())
-time.sleep(1)
+print
 
-print("Build G2, which is identical to G1")
+print("Run G2, which is identical to G1")
 a = mx.nd.ones(2)
 b = mx.nd.ones(2)
 c = a + b
 print(c.asnumpy())
-time.sleep(1)
+print
 
-print("Build G3, which uses diff op than G1 & G2")
+print("Run G3, which uses diff op than G1 & G2")
 a = mx.nd.ones(2)
 b = mx.nd.ones(2)
 c = a - b
 print(c.asnumpy())
-time.sleep(1)
+print
 
-print("Build G4, which uses diff compute order than G3")
+print("Run G4, which uses diff compute order than G3")
 a = mx.nd.ones(2)
 b = mx.nd.ones(2)
 c = b - a
 print(c.asnumpy())
-time.sleep(1)
+print
+
+print("Run G5, which size is different from G1")
+a = mx.nd.ones(3)
+b = mx.nd.ones(3)
+c = a + b
+print(c.asnumpy())
+print
