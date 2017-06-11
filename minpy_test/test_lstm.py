0fa12ee3265cefb53ff0cfb96e9c2e63fb22e99e
Yutian Li <hotpxless@gmail.com>
2017-06-10 18:15:25 -0700

    [jit] Wrapper function.

--- minpy_test/test_lstm.py
+++ minpy_test/test_lstm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import time
+import functools
 import mxnet as mx
 import mxnet.ndarray as nd
 
@@ -51,6 +52,55 @@ b = nd.zeros(shape=(10, ))
 N = 128
 X = gaussian(shape=(N, 784 // 7, 7))
 
+
+def pure(func):
+    '''
+    Assumptions:
+    1. Closed (pure) function.
+    2. Arguments are of type NDArray.
+    3. Return value is either NDArray of an iterable of NDArrays.
+    4. No side effect.
+    '''
+    closure_payload = {
+        'segment_id': None,
+        'output_shapes': None,
+    }
+
+    # fmap :: (a -> b) -> Either a [a] -> Either b [b]
+    def fmap(f, value):
+        if isinstance(value, mx.nd.NDArray):
+            return f(value)
+        else:
+            return list(map(f, value))
+
+    @functools.wraps(func)
+    def wrapper(*args):
+        if closure_payload['segment_id'] is None:
+            mx.minpy.start_trace()
+            ret = func(*args)
+            fmap(lambda x: mx.minpy.mark_as_output(x), ret)
+            segment_id = mx.minpy.end_trace()
+            closure_payload['segment_id'] = segment_id
+            closure_payload['output_shapes'] = fmap(lambda x: x.shape, ret)
+        ret_value = fmap(lambda x: mx.nd.zeros(shape=x.shape), closure_payload['output_shapes'])
+        run_segment(segment_id, args, closure_payload, ret_value)
+        return ret_value
+
+    return wrapper
+
+
+@pure
+def foo(h, c, patch, Wxi, Wxf, Wxo, Wxg, bxi, bxf, bxo, bxg, Whi, Whf, Who,
+        Whg, bhi, bhf, bho, bhg):
+    i = sigmoid(linear(patch, Wxi, bxi) + linear(h, Whi, bhi))
+    f = sigmoid(linear(patch, Wxf, bxf) + linear(h, Whf, bhf))
+    o = sigmoid(linear(patch, Wxo, bxo) + linear(h, Who, bho))
+    g = nd.tanh(linear(patch, Wxg, bxg) + linear(h, Whg, bhg))
+    c = f * c + i * g
+    h = o * mx.nd.tanh(c)
+    return h, c, linear(h, W, b)
+
+
 for index in range(10):
     h = nd.zeros((N, D))
     c = nd.zeros((N, D))
@@ -58,11 +108,18 @@ for index in range(10):
     for i in range(784 // 7):
         patch = mx.nd.slice_axis(X, axis=1, begin=i, end=(i + 1))
         t0 = time.time()
-        i = sigmoid(linear(patch, Wxi, bxi) + linear(h, Whi, bhi))
-        f = sigmoid(linear(patch, Wxf, bxf) + linear(h, Whf, bhf))
-        o = sigmoid(linear(patch, Wxo, bxo) + linear(h, Who, bho))
-        g = nd.tanh(linear(patch, Wxg, bxg) + linear(h, Whg, bhg))
-        c = f * c + i * g
-        h = o * mx.nd.tanh(c)
-        print((time.time() - t0) / (index + 1))
-        linear(h, W, b).asnumpy()
+        # Segment start.
+        # i = sigmoid(linear(patch, Wxi, bxi) + linear(h, Whi, bhi))
+        # f = sigmoid(linear(patch, Wxf, bxf) + linear(h, Whf, bhf))
+        # o = sigmoid(linear(patch, Wxo, bxo) + linear(h, Who, bho))
+        # g = nd.tanh(linear(patch, Wxg, bxg) + linear(h, Whg, bhg))
+        # c = f * c + i * g
+        # h = o * mx.nd.tanh(c)
+        # linear(h, W, b).asnumpy()
+        # Segment end.
+        h_new, c_new, res = foo(h, c, patch, Wxi, Wxf, Wxo, Wxg, bxi, bxf, bxo,
+                                bxg, Whi, Whf, Who, Whg, bhi, bhf, bho, bhg)
+        # res.asnumpy()
+        h = h_new
+        c = c_new
+        print(time.time() - t0)
